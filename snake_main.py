a54415b163bf0c1559507a19f31c612513c54622
John <98199268+jecko5150@users.noreply.github.com>
2022-12-12 11:46:06 -0500

    Update snake_main.py

--- snake_main.py
+++ snake_main.py
@@ -1,11 +1,11 @@
 import time
 from turtle import Screen
 
-
-
 from scoreboard import Scoreboard, Countdown, PlayerName
 from snake import Snake
 from food import Food
+
+game_is_on = True
 
 screen = Screen()
 screen.setup(600, 600)
@@ -13,22 +13,20 @@ screen.bgcolor('black')
 screen.title("Snakes!!!")
 screen.tracer(0)
 
-
-
 snake = Snake()
 food = Food()
 player = PlayerName()
-scoreboard = Scoreboard()
 countdown = Countdown()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
 screen.onkey(key="Left", fun=snake.left)
 screen.onkey(key="Right", fun=snake.right)
+screen.onkey(key="1", fun=screen.resetscreen)
 
-game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(.1)
@@ -54,13 +52,10 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-#     Detect Snake Body
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    #     Detect Snake Body
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
-
 
 screen.mainloop()
